55f27c061254bdc9948747cdd160334c51097bae
Zuglya <irussel86@gmail.com>
2024-11-01 19:23:50 +0400

    Lesson 19

--- Lesson 19/src/Electro.java
+++ Lesson 19/src/Electro.java
@@ -1,2 +1,12 @@
-public class Electro {
+public class Electro extends Car {
+    public Electro() {
+    }
+
+    public Electro(String mark, Double engineVolume, Integer maxSpeed) {
+        super(mark, engineVolume, maxSpeed);
+    }
+
+    public void move() {
+        System.out.println("Электрокар движется со скоростью " + super.getMaxSpeed());
+    }
 }
